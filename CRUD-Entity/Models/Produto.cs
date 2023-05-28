69ca93b3f9fc49d03e90d70d413e694d157aa4c6
leogo <leogo@DESKTOP-2GIKF6G>
2023-05-28 08:38:22 -0300

    Adicionar arquivos de projeto.

---
+++ CRUD-Entity/Models/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_Entity.Models
+{
+    [Table("Produto")]
+    public class Produto
+    {
+        
+        [Column("Id")]
+        [Display(Name = "Codigo")]
+        public int Id { get; set; }
+
+        [Column("Nome")]
+        [Display(Name = "Nome")]
+        public String Nome { get; set; }
+
+    }
+}
---
+++ CRUD-Entity/Program.cs
@@ -0,0 +1,31 @@
+using CRUD_Entity.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<Contexto>(options => options.UseSqlServer
+("Server=DESKTOP-2GIKF6G\\SQLSERVER;\r\nDataBase=CRUD_MVC_SQL;Trusted_Connection=True;TrustServerCertificate=true"));
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
